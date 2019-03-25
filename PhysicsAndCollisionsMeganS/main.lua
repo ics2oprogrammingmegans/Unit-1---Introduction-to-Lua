de62b06d4acf89cc3ef6f3cc04be5eb438239149
Megan Souannhaphanh <47784122+s256420@users.noreply.github.com>
2019-03-25 09:15:11 -0400

    Added the image folder

--- PhysicsAndCollisionsMeganS/main.lua
+++ PhysicsAndCollisionsMeganS/main.lua
@@ -1,7 +1,23 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: PhysicsAndCollisions
+-- Name: Megan.S
+-- Course: ICS2O
+-- This program
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--Hide the status bar on the iPad Air
+display.setStatusBar(display.HiddenStatusBar)
+
+--Load physics
+local physics = require("physics")
+
+--Start physics
+physics.start()
+-----------------------------------------------------------
+--OBJECTS--
+-----------------------------------------------------------
+
+--Create the ground and set its x and y 
+ local ground = display.newImage("Physics/ground.png", 0 , 0)
+ ground.x = display.contentWidth/2
+ ground.y = display.contentHeight*9/10
